08ba9f789bc27c39d688207acfa9e77a5554be4c
Deyvid William <deyvidwms@gmail.com>
2022-09-21 18:15:09 -0300

    Fazendo a listagem dos pratos

--- frontend/src/paginas/Administracao/PaginaBaseAdmin.tsx
+++ frontend/src/paginas/Administracao/PaginaBaseAdmin.tsx
@@ -17,9 +17,22 @@ const PaginaBaseAdmin = () => {
                   Restaurantes
                 </Button>
               </Link>
+
               <Link component={RouterLink} to="/admin/restaurantes/novo">
                 <Button sx={{ my: 2, color: "white" }}>
                   Novo Restaurantes
+                </Button>
+              </Link>
+
+              <Link component={RouterLink} to="/admin/pratos">
+                <Button sx={{ my: 2, color: "white" }}>
+                  Pratos
+                </Button>
+              </Link>
+
+              <Link component={RouterLink} to="/admin/pratos/novo">
+                <Button sx={{ my: 2, color: "white" }}>
+                  Novo Prato
                 </Button>
               </Link>
             </Box>
