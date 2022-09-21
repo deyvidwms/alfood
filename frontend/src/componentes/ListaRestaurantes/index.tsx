9323f01a4301245a57f3c82123a8700f644f0d81
Deyvid William <deyvidwms@gmail.com>
2022-09-21 15:46:36 -0300

    Refatoração das telas e das requisições

--- frontend/src/componentes/ListaRestaurantes/index.tsx
+++ frontend/src/componentes/ListaRestaurantes/index.tsx
@@ -1,18 +1,28 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
+// esses são os possíveis parâmetros que podemos enviar para a API
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
+
 const ListaRestaurantes = () => {
 
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
-  const [proximaPagina, setProximaPagina] = useState('');
-  const [paginaAnterior, setPaginaAnterior] = useState('');
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [proximaPagina, setProximaPagina] = useState('')
+  const [paginaAnterior, setPaginaAnterior] = useState('')
 
-  const carregarDados = (url: string) => {
-    axios.get<IPaginacao<IRestaurante>>(url)
+  const [busca, setBusca] = useState('')
+
+  // agora, o carregarDados recebe opcionalmente as opções de configuração do axios
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes)
       .then(resposta => {
         setRestaurantes(resposta.data.results)
         setProximaPagina(resposta.data.next)
@@ -23,23 +33,31 @@ const ListaRestaurantes = () => {
       })
   }
 
-  useEffect(() => {
-    carregarDados('http://localhost:8000/api/v1/restaurantes/')
-  }, []);
+  // a cada busca, montamos um objeto de opções
+  const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    const opcoes = {
+      params: {
 
-  const verMais = () => {
-    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
-      .then(resposta => {
-        setRestaurantes([...restaurantes, ...resposta.data.results])
-        setProximaPagina(resposta.data.next)
-      })
-      .catch(erro => {
-        console.log(erro)
-      })
+      } as IParametrosBusca
+    }
+    if (busca) {
+      opcoes.params.search = busca
+    }
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
   }
+
+  useEffect(() => {
+    // obter restaurantes
+    carregarDados('http://localhost:8000/api/v1/restaurantes/')
+  }, [])
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <form onSubmit={buscar}>
+      <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)} />
+      <button type='submit'>buscar</button>
+    </form>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
       Página Anterior
--- frontend/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ frontend/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,31 +1,65 @@
-import { Button, TextField } from "@mui/material";
-import axios from "axios";
-import { useState } from "react";
+import { Button, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import http from "../../../http";
+import IRestaurante from "../../../interfaces/IRestaurante";
 
 const FormularioRestaurante = () => {
+
+  const parametros = useParams();
+
+  useEffect(() => {
+    if (parametros.id) {
+      http.get<IRestaurante>(`restaurantes/${parametros.id}/`)
+        .then(resposta => setNomeRestaurante(resposta.data.nome))
+    }
+  }, [parametros]);
 
   const [nomeRestaurante, setNomeRestaurante] = useState('');
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
-    axios.post('http://localhost:8000/api/v2/restaurantes/', {
-      nome: nomeRestaurante
-    })
-      .then(()=>{
-        alert('Restaurante cadastrado')
+
+    if (parametros.id) {
+      http.put(`restaurantes/${parametros.id}/`, {
+        nome: nomeRestaurante
       })
+        .then(()=>{
+          alert('Restaurante atualizado')
+        })
+    } else {
+      http.post('restaurantes/', {
+        nome: nomeRestaurante
+      })
+        .then(()=>{
+          alert('Restaurante cadastrado')
+        })
+    }
   };
 
   return (
-    <form onSubmit={aoSubmeterForm}>
-      <TextField 
-        value={nomeRestaurante} 
-        onChange={ evento => setNomeRestaurante(evento.target.value) } 
-        label="Nome do restaurante" 
-        variant="standard"
-      />
-      <Button type="submit" variant="outlined">Salvar</Button>
-    </form>
+    <Box sx={{display: 'flex', flexDirection: "column", alignItems: "center"}}>
+      <Typography component="h1" variant="h6">Formulário de Restaurantes</Typography>
+      <Box component="form" onSubmit={aoSubmeterForm}>
+        <TextField 
+          value={nomeRestaurante} 
+          onChange={ evento => setNomeRestaurante(evento.target.value) } 
+          label="Nome do restaurante" 
+          variant="standard"
+          fullWidth
+          required
+        />
+        <Button 
+          sx={{ marginTop: 1 }}
+          type="submit" 
+          variant="outlined"
+          fullWidth
+        >
+          Salvar
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
